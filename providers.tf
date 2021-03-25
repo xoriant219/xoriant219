d23f149022044ad5686dd7aac5afd8fde0a86fde
xoriant219 <80835383+xoriant219@users.noreply.github.com>
2021-03-25 11:53:37 +0530

    Delete providers.tf

--- providers.tf
+++
@@ -1,8 +0,0 @@
-provider "azurerm" {
-    client_id = "1d840a42-d149-4535-9fdc-2727ee72bfef"
-    client_secret = "myTOjs.PyJIo4xt505Ty389g3Q1e.6k~i~"
-    tenant_id = "94f5219d-c94c-4926-9666-901768cb1691"
-    subscription_id = "93cc419e-beba-43a1-bbd8-efb7a29467fc"
-
-features {}
-}
